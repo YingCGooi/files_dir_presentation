f4b6463258f0a42da8460c1731e605f41dba7c36
YingCGooi <ying.c.gooi@gmail.com>
2017-10-21 14:14:14 -0500

    updating project.rb to contain filenames

---
+++ example_project/test/project_test.rb
@@ -0,0 +1,4 @@
+'hello world2'# this is a test.'File.write("test/project_test.rb", string, { mode: 'a+'})File.write("test/project_test.rb", string, { mode: 'a+'}
+)# Adding new text# Adding new text'this is a test'
+'this is a test too'
+'this is a test three'
